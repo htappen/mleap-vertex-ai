69c76ba8627d5997d61d1c51d83aea17f93ebb65
Henry Tappen <htappen@google.com>
2021-09-23 05:09:06 +0000

    Code is done, now needs testing

--- nginx/edit_output_body.lua
+++ nginx/edit_output_body.lua
@@ -1,2 +1,21 @@
-local out_body = ngx.arg[1]
-ngx.arg[1] = out_body:gsub("rows", "predictions", 1)
+local cjson = require "cjson"
+local output_key_name = os.getenv("OUTPUT_KEY")
+
+local mleap_json = cjson.decode(ngx.arg[1])
+local key_position = 0
+for i, schema_field in ipairs(mleap_json.schema) do
+    if schema_field.name == output_key_name then
+        key_position = i
+        break
+    end
+end
+
+-- TODO: error handling, what to do if it's not found? Can I set an error response?
+local predictions_out = {}
+for i, row in ipairs(mleap_json.rows) do
+    predictions_out[i] = row[key_position]
+end
+local json_out = { ["predictions"]=predictions_out }
+
+-- TODO: if there are no rows, will cjson treat it as an empty array?
+ngx.arg[1] = cjson.encode(json_out)
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,10 +1,26 @@
+# Check that necessary config is set
+# TODO: add the check
+
 # Download the model
+mkdir -p /root/models/
+gsutil cp $AIP_STORAGE_URI /root/models/
+ls *.zip | head -n 1 | xargs mv {} /root/model.zip
 
 # Download the schema
+mkdir -p /root/schema/
+gsutil cp $MLEAP_SCHEMA_URI /root/schema/
+ls *.zip | head -n 1 | xargs mv {} /root/input_schema.json
 
 # Start mleap
+/opt/docker/bin/mleap-spring-boot &
+
+sleep 20
 
 # Load the model in mleap
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"modelName":"model","uri":"file:/root/model.zip","config":{"memoryTimeout":900000,"diskTimeout":900000},"force":false}' \
+  http://localhost:65237/models
 
 # Start nginx
 nginx -c /etc/nginx/nginx.conf
