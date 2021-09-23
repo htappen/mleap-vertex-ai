ddb6c2d429cfe159b21a68f3df0b062892c1a78f
Henry Tappen <htappen@google.com>
2021-09-23 22:36:07 +0000

    Getting output filter to work

--- nginx/edit_output_body.lua
+++ nginx/edit_output_body.lua
@@ -1,21 +1,23 @@
 local cjson = require "cjson"
 local output_key_name = os.getenv("OUTPUT_KEY")
 
-local mleap_json = cjson.decode(ngx.arg[1])
-local key_position = 0
-for i, schema_field in ipairs(mleap_json.schema) do
-    if schema_field.name == output_key_name then
-        key_position = i
-        break
+-- TODO: not really going to work with large responses. Need to deal with chunking
+if not ngx.arg[2] then
+    local mleap_json = cjson.decode(ngx.arg[1])
+    local key_position = 0
+    for i, schema_field in ipairs(mleap_json.schema.fields) do
+        if schema_field.name == output_key_name then
+            key_position = i
+            break
+        end
     end
-end
 
--- TODO: error handling, what to do if it's not found? Can I set an error response?
-local predictions_out = {}
-for i, row in ipairs(mleap_json.rows) do
-    predictions_out[i] = row[key_position]
-end
-local json_out = { ["predictions"]=predictions_out }
+    -- TODO: error handling, what to do if it's not found? Can I set an error response?
+    local predictions_out = {}
+    for i, row in ipairs(mleap_json.rows) do
+        predictions_out[i] = row[key_position]
+    end
+    local json_out = { ["predictions"]=predictions_out }
 
--- TODO: if there are no rows, will cjson treat it as an empty array?
-ngx.arg[1] = cjson.encode(json_out)
+    ngx.arg[1] = cjson.encode(json_out)
+end
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,9 @@
 # Check that necessary config is set
 # TODO: add the check
 
+# Start mleap
+/opt/docker/bin/mleap-spring-boot &
+
 # Download the model
 mkdir -p /root/models/
 gsutil cp -r $AIP_STORAGE_URI /root/models/
@@ -11,16 +14,13 @@ mkdir -p /root/schema/
 gsutil cp $MLEAP_SCHEMA_URI /root/schema/
 find /root/schema -name "*json" | head -n 1 | xargs -I{} mv {} /root/input_schema.json
 
-# Start mleap
-/opt/docker/bin/mleap-spring-boot &
-
-sleep 20
-
 # Load the model in mleap
+sleep 10
+
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{"modelName":"model","uri":"file:/root/model.zip","config":{"memoryTimeout":900000,"diskTimeout":900000},"force":false}' \
-  http://localhost:65237/models
+  http://localhost:65327/models
 
 # Start nginx
 nginx -c /etc/nginx/nginx.conf
